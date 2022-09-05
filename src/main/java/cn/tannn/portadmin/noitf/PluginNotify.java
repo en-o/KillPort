4ffc1a1e44bcb92a39c19203879119d0fd5aeffa
tan <1445763190@qq.com>
2022-09-05 14:06:05 +0800

    fix: 优化代码 1. NotificationGroup.<init>(...) (1) （计划在 未来的版本中删除）

--- src/main/java/cn/tannn/portadmin/noitf/PluginNotify.java
+++ src/main/java/cn/tannn/portadmin/noitf/PluginNotify.java
@@ -1,31 +1,34 @@
 package cn.tannn.portadmin.noitf;
 
-import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationGroupManager;
-import com.intellij.notification.Notifications;
-import com.intellij.openapi.extensions.PluginId;
+import com.intellij.notification.impl.NotificationGroupEP;
+import com.intellij.openapi.project.Project;
+import groovyjarjarantlr4.v4.runtime.misc.Nullable;
 
 /**
  * 消息通知
+ *
  * @author tn
  */
 public class PluginNotify {
 
 
-    public  static  void notification(NotifyEntity notify) {
+    public static void notification(@Nullable Project project,
+                                    NotifyEntity notify) {
+
         /**
          * 弹出通知
          * 1. id
          * 2. 弹出的消息样式： BALLOON 气泡
          * 3. 事件记录 (右下角中的event log 中是否记录)
          */
-        NotificationGroup notificationGroup = new NotificationGroup(notify.getPluginId(),
-                notify.getNotificationDisplayType(),
-                notify.isLogBuDefault());
-        /*  消息 + 消息的类型 */
-        Notification notification = notificationGroup.createNotification(notify.getMessages(), notify.getMessageType());
-        /* 显示 */
-        Notifications.Bus.notify(notification);
+        NotificationGroupManager.getInstance()
+                /* id  */
+                .getNotificationGroup(notify.getPluginId())
+                /*  消息 + 消息的类型 */
+                .createNotification(notify.getMessages(), notify.getNotificationType())
+                /* 显示 */
+                .notify(project);
     }
 }
